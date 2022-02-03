12edb8b6e120799480a29bd9ad1e9c979514bc2e
Ele Na <elena.china-kolehmainen@edu.bc.fi>
2022-02-03 23:26:40 +0200

    all directions

--- robot.py
+++ robot.py
@@ -3,8 +3,8 @@ dot_count = 50
 direction = 'up'
 
 def isFree(lines, x, y):
-    print(x,y)
-    print(lines[x][y])
+    # print(x,y)
+    # print(lines[x][y])
     if (x >= 0) & (y >= 0) & (x < line_count) & (y < dot_count) & (lines[x][y] != '#'):
         return True
     else:
@@ -13,67 +13,79 @@ def isFree(lines, x, y):
 def turnRight(direction):
     if direction == 'up':
         return 'R'
-    elif direction == 'R':
+    if direction == 'R':
         return 'down'
-    elif direction == 'down':
+    if direction == 'down':
         return 'L'
-    elif direction == 'L':
+    if direction == 'L':
         return 'up'
 
 def moveForward(lines, x, y, direction, steps):
     # print(steps)
+    # print('direction:', direction)
     # base case: E found
-    if x == 'E':
+    if lines[x][y] == 'E':
+        print('total steps:', steps)
         return True
 
     if direction == 'up':
         if isFree(lines, x-1, y):
             steps += 1
-            print(steps)
+            # print('step n:', steps)
             if moveForward(lines, x-1, y, direction, steps):
                 return True
         else:
-            turnRight(direction)
+            direction = turnRight(direction)
+            steps += 1
+            if moveForward(lines, x, y+1, direction, steps):
+                return True
 
     if direction == 'R':
         if isFree(lines, x, y+1):
             steps += 1
-            # print(steps)
-            if moveForward(lines, x, y+1, direction):
+            # print('step n:', steps)
+            if moveForward(lines, x, y+1, direction, steps):
                 return True
         else:
-            turnRight(direction)
+            direction = turnRight(direction)
+            steps += 1
+            if moveForward(lines, x+1, y, direction, steps):
+                return True
 
     if direction == 'down':
         if isFree(lines, x+1, y):
             steps += 1
-            # print(steps)
-            if moveForward(lines, x+1, y, direction):
+            # print('step n:', steps)
+            if moveForward(lines, x+1, y, direction, steps):
                 return True
         else:
-            turnRight(direction)
+            direction = turnRight(direction)
+            steps += 1
+            if moveForward(lines, x, y-1, direction, steps):
+                return True
 
     if direction == 'L':
         if isFree(lines, x, y-1):
             steps += 1
-            # print(steps)
-            if moveForward(lines, x, y-1, direction):
+            # print('step n:', steps)
+            if moveForward(lines, x, y-1, direction, steps):
                 return True
         else:
-            turnRight(direction)
+            direction = turnRight(direction)
+            steps += 1
+            if moveForward(lines, x-1, y, direction, steps):
+                return True
 
 
-with open('field.txt', 'r') as f:
+with open('field3275647.txt', 'r') as f:
     text = f.readlines()
     lines = text[1:]
 
     # S and E coordinates initialized with zero
-    s_i = 0
-    s_j = 0
-    e_i = 0
-    e_j = 0
-    now_i = 0
-    now_j = 0
+    # s_i = 0
+    # s_j = 0
+    # e_i = 0
+    # e_j = 0
 
     # find S and E coordinates
     for i, l in enumerate(lines):
@@ -83,14 +95,13 @@ with open('field.txt', 'r') as f:
                     s_i = i
                     s_j = j
                     # print('S found in line {}, index {}'.format(i, j))
-        if 'E' in l:
-            for j, step in enumerate(l):
-                if step == 'E':
-                    e_i = i
-                    e_j = j
+        # if 'E' in l:
+        #     for j, step in enumerate(l):
+        #         if step == 'E':
+        #             e_i = i
+        #             e_j = j
                     # print('E found in line {}, index {}'.format(i, j))
-    now_i = s_i
-    now_j = s_j
+
     # print(s_i, s_j)
     # print(lines[s_i][s_j])
     # print(e_i, e_j)
